ec0df8745a9fec14c72490a0eb65610b9c8cf05f
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-10-19 22:43:01 +0530

    Day6 Assignment2

---
+++ Day6Assignment2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day6Assignment2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInput Input = new UserInput();
+            //Input.vendingMachineProblem(); // Calling Vending Machine Problem
+            Input.dayOfWeekProblem(); // Calling Day Of Week Problem
+        }
+    }
+}
---
+++ Day6Assignment2/UserInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment2
+{
+    // Vending Machine Problem
+    public class UserInput
+    {
+        public void vendingMachineProblem()
+        {
+            var vm = VendingMachine.UserInputForVendingMachine();
+            VendingMachine machine = new VendingMachine(vm.Number);
+            machine.calculateChange();
+                
+            
+        }
+        // Day Of Week Problem
+        public void dayOfWeekProblem()
+        {
+            var w = DayOfWeek.UserInputForDayOfWeek();
+            DayOfWeek week = new DayOfWeek(w.Date,w.Month,w.Year);
+            week.FindDayOfWeek();
+        }
+    }
+}
